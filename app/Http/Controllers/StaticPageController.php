a223e8c7519f018cd434f5c06f4958fbc251c0d3
acarolinacc <acarolinacc2003@gmail.com>
2023-11-20 01:03:34 +0000

    Static Pages controller

---
+++ app/Http/Controllers/StaticPageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class StaticPageController extends Controller
+{
+    public function faq()
+    {
+        return view('static.faq');
+    }
+
+    public function about()
+    {
+        return view('static.about');
+    }
+
+    public function privacy()
+    {
+        return view('static.privacy');
+    }
+}
