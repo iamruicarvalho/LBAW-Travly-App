c070f42df6fb4893491d026f1861b26eeca5d8b0
Rui Carvalho <ruipedrobc@gmail.com>
2023-11-21 03:15:08 +0000

    changed RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,46 +3,68 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
-
-use Illuminate\View\View;
-
+use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
+    // only guests (unauthenticated users) can access the registration form
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
     /**
      * Display a login form.
      */
-    public function showRegistrationForm(): View
+    public function showRegistrationForm()
     {
         return view('auth.register');
     }
 
     /**
+     * Validate user inputs.
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name_' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
+            'email' => 'required|email|max:255|unique:users',
+            'password_' => 'required|string|min:8|confirmed',
+        ]);
+    }
+
+    /**
      * Register a new user.
      */
-    public function register(Request $request)
+    protected function create(array $data)
     {
-        $request->validate([
-            'name' => 'required|string|max:250',
-            'email' => 'required|email|max:250|unique:users',
-            'password' => 'required|min:8|confirmed'
+        return User::create([
+            'name_' => $data['name'],
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password_' => Hash::make($data['password']),
         ]);
 
-        User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]);
+        $credentials = [
+            'email' => $data['email'],
+            'password' => $data['password'],
+        ];
 
-        $credentials = $request->only('email', 'password');
-        Auth::attempt($credentials);
-        $request->session()->regenerate();
-        return redirect()->route('cards')
-            ->withSuccess('You have successfully registered & logged in!');
+        // Attempt to log in
+        if (Auth::attempt($credentials)) {
+            session_regenerate_id();
+            // $request->session()->regenerate();
+
+            return redirect()->route('/')->with('success', 'You have successfully registered and logged in!');
+        }
+
+        // If login fails, handle it accordingly
+        return redirect()->route('/register')
+            ->with('error', 'Failed to register. Try again.');
     }
 }
