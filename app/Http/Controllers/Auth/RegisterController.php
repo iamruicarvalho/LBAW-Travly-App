f9972b4a9abcb7481e5674d67d532c16c133dc0c
Rui Carvalho <ruipedrobc@gmail.com>
2023-11-22 00:48:49 +0000

    fixed bug in RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,19 +3,17 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View; // Adicione esta linha para importar a classe View
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
-
-use Illuminate\View\View;
-
+use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
     /**
-     * Display a login form.
+     * Display a registration form.
      */
     public function showRegistrationForm(): View
     {
@@ -28,20 +26,20 @@ class RegisterController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'name_' => 'required|string|max:250',
-            'username' => 'required|string|max:250|unique:users',
-            'email' => 'required|email|max:250|unique:users',
-            'password_' => 'required|min:8|confirmed'
+            'name' => 'required|string|max:255',
+            'username' => 'required|string|unique:user,username', 
+            'email' => 'required|email|unique:user,email',
+            'password' => 'required|min:8|confirmed',
         ]);
 
-        User::create([
+        User::create([   
             'name_' => $request->name,
             'username' => $request->username,
             'email' => $request->email,
-            'password_' => Hash::make($request->password)
+            'password_' => Hash::make($request->password),
         ]);
 
-        $credentials = $request->only('email', 'password_');
+        $credentials = $request->only('email', 'password');
         Auth::attempt($credentials);
         $request->session()->regenerate();
         return redirect()->route('home')
